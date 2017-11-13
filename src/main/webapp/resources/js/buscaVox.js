8cf525fa6cd7efaabd93bb27b2ec9c42f90500db
NoubMaster <noubmaster@gmail.com>
2017-11-13 14:40:25 -0200

    fazendo comandos de voz

--- src/main/webapp/resources/js/buscaVox.js
+++ src/main/webapp/resources/js/buscaVox.js
@@ -3,18 +3,22 @@ function vozes() {
     var textInput = document.getElementById("busca:txt");
 
 
-        annyang.addCallback('result', function (phrases) {
-            if(phrases[0]==='search' ||
-                    phrases[0]!==' search'){
-                console.log('foda22222');
-                textInput.value = phrases[0];
-                console.log("I think the user said: ", phrases[0]);
-                console.log("But then again, it could be any of the following: ", phrases);
-            } else{
-                console.log('search');
-                
+    annyang.addCallback('result', function (phrases) {
+        if (phrases[0] === 'search' ||
+                phrases[0] === ' search' ||
+                phrases[0] === 'none' ||
+                phrases[0] === ' none') {
+            console.log('search');
+            if(phrases[0] === 'none' ||
+                phrases[0] === ' none') {
+            textInput.value = null;
             }
-        });
+        } else {
+            textInput.value = phrases[0];
+            console.log("I think the user said: ", phrases[0]);
+            console.log("But then again, it could be any of the following: ", phrases);
+        }
+    });
 
 
 }
